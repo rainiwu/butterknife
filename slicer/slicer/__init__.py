f5a0e35e3f4db82ee91b9c46f64691a70b4e8b50
Jiawen Xu <jix034@ucsd.edu>
2022-11-20 13:49:14 -0800

    update slicer

--- slicer/slicer/__init__.py
+++ slicer/slicer/__init__.py
@@ -8,8 +8,9 @@ OUTPUT_SIZE = 5
 LEARNING_RATE = 0.1
 DISCOUNT_RATE = 0.95
 NUM_EPISODES =  2000
+k = 0.4 # ratio of mean to variance for reward function
 def __init__():
-    env = Environment(INPUT_SIZE)
+    env = Environment(INPUT_SIZE, k)
     model = DQN(INPUT_SIZE, OUTPUT_SIZE)
     optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
     loss_function = torch.nn.MSELoss()
