d905e98752c7fd1e3254ebd3acacaac3c6560d58
Raini Wu <rainiwu01@gmail.com>
2022-11-22 14:06:58 -0800

    finish initial implementation of server

--- toast/toast/server.py
+++ toast/toast/server.py
@@ -31,7 +31,7 @@ class Server:
         self.client_ip: int = 2
 
         # dict with key of client_ip, and value of (to_sock, from_sock)
-        self.client_sockets: Dict[int, Tuple[zmq.Socket, zmq.Socket]] = {}
+        self.client_sockets: Dict[str, Tuple[zmq.Socket, zmq.Socket]] = {}
 
     def handle_handshake(self) -> None:
         """
@@ -44,17 +44,27 @@ class Server:
         )
         self.client_ip += 1
 
-    def process_incoming(self) -> None:
+    def process_outgoing(self) -> None:
         """
         reads a packet from the interface and sends it to the appropriate client
         """
-        pass
+        payload = self.tun.read(self.tun.MTU)
+        temp = bytearray(payload)
+        # strip the ipv4 packet to get dest addr
+        dest: str = "".join(str(val) + "." for val in temp[20:24])[0:-1]
+        if dest in self.client_sockets.keys():
+            self.client_sockets[dest][0].send_string(payload)
+            self.client_sockets[dest][0].recv()
 
-    def process_outgoing(self) -> None:
+    def process_incoming(self) -> None:
         """
         gets all packets from all clients, and sends it to the interface
         """
-        pass
+        for val in self.client_sockets.values():
+            if val[1].poll(timeout=0.01):
+                payload = val[1].recv_string()
+                self.tun.write(payload)
+                val[1].send_string("")
 
     def __initialize_client(self, client_ip: int) -> None:
         """
@@ -62,14 +72,14 @@ class Server:
 
         :param client_ip: unique id of a new client
         """
-        self.client_sockets[client_ip] = (
+        self.client_sockets[self.CLIENT_IP_PREFIX + str(client_ip)] = (
             self.context.socket(zmq.REQ),  # to client sock
             self.context.socket(zmq.REP),  # from client sock
         )
-        self.client_sockets[client_ip][0].bind(
+        self.client_sockets[self.CLIENT_IP_PREFIX + str(client_ip)][0].bind(
             self.IPC_PREFIX + self.CLIENT_IP_PREFIX + str(client_ip) + "_tocli"
         )
-        self.client_sockets[client_ip][1].bind(
+        self.client_sockets[self.CLIENT_IP_PREFIX + str(client_ip)][1].bind(
             self.IPC_PREFIX
             + self.CLIENT_IP_PREFIX
             + str(client_ip)
