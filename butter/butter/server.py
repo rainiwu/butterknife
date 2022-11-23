1f482abac06a50bf4aca4edcdf6ec354ba46ce57
j613 <jimmyvillani@gmail.com>
2022-11-22 20:41:38 -0800

    Committing Server.py

--- butter/butter/server.py
+++ butter/butter/server.py
@@ -1,32 +1,43 @@
 #
-#   Hello World server in Python
-#   Binds REP socket to tcp://*:5555
-#   Expects b"Hello" from client, replies with b"World"
+#   Video Server
 #
 
 import time
+import numpy
+import string
+import os
 import zmq
+import manifest
+import re
 
 MAN_REQ = "GET manifest"
-INIT_REQ = "GET init
+INIT_REQ = "GET init"
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
+
+# Dummy values for 100 second video. 
+chunks = 25
+framesPerChunk = 120
+frames = 30
+# Average bytes per frame to be 10e3.
+bytesPerFrame = 10000
+
+
+# QoE 
+QoE = 0
+
+
 while True:
     #  Wait for next request from client
     message = socket.recv()
 
-    if(message == MAN_REQ):
+    if message == MAN_REQ:
+        mani = manifest(chunks, framesPerChunk, frames)
+        socket.send_pyobj(mani)
+    elif INIT_REQ in message:
+        socket.send(os.urandom(framesPerChunk*bytesPerFrame))
+        QoE = re.findall("\d+", message)[0]
         
-
-
-
-    print(f"Received request: {message}")
-
-    #  Do some 'work'
-    time.sleep(1)
-
-    #  Send reply back to client
-    socket.send_string("World")
