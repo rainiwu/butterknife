17abbad8fa373661af2c5061940800796c0b9777
j613 <jimmyvillani@gmail.com>
2022-11-22 21:40:37 -0800

    Adding serverside code to handle devices

--- butter/butter/server.py
+++ butter/butter/server.py
@@ -10,7 +10,8 @@ import zmq
 import manifest
 import re
 
-MAN_REQ = "GET manifest"
+MAN_REQ_BUFF = "GET manifest buffered"
+MAN_REQ_UNBUFF = "GET manifest unbuffered"
 BUFF_REQ = "GET buffered"
 UNBUFF_REQ = "GET unbuffered"
 
@@ -22,21 +23,31 @@ socket.bind("tcp://*:5555")
 # Dummy values for 100 second video. 
 chunks = 25
 framesPerChunk = 120
-frames = 30
+framesPerSecond = 30
 # Average bytes per frame to be 10e3.
 bytesPerFrame = 10000
 
 
 # QoE 
+QoEunbuff = 100
+QoEbuff = 100
 
 
 while True:
     #  Wait for next request from client
     message = socket.recv()
 
-    if message == MAN_REQ:
-        mani = manifest(chunks, framesPerChunk, frames)
+    if message == MAN_REQ_UNBUFF
+        # For buffered video, send the buffered manifest. 
+        mani = manifest(chunks, 1, framesPerSecond, 0)
+        socket.send_pyobj(mani)
+    elif message == MAN_REQ_BUFF
+        # For buffered video, send the buffered manifest. 
+        mani = manifest(chunks, framesPerChunk, framesPerSecond, 1)
         socket.send_pyobj(mani)
     elif UNBUFF_REQ in message:
+        socket.send(os.urandom(1*bytesPerFrame))
+        QoEunbuff = re.findall("\d+", message)[0]
+    elif BUFF_REQ in message:
         socket.send(os.urandom(framesPerChunk*bytesPerFrame))
-        QoE = re.findall("\d+", message)[0]
+        QoEunbuff = re.findall("\d+", message)[0]
