7567f05a7423c9ea35d0231cebdfca2d405d3dfe
Jiawen Xu <jix034@ucsd.edu>
2022-11-22 21:17:21 -0800

    update client buffered:

--- butter/butter/client_buf.py
+++ butter/butter/client_buf.py
@@ -2,12 +2,10 @@ import zmq
 from manifest import manifest
 import time
 
-MAN_REQ = "GET manifest"
-BUFF_REQ = "GET buffered"
 BUFFER_SIZE = 100
 
 class client_buffer():
-    def __init__(self, address="tcp://localhost:5555", buffer_size=BUFFER_SIZE):
+    def __init__(self, address="tcp://localhost:5555", buffer_size=100):
         self.context = zmq.Context()
         #  Socket to talk to server
         self.socket = self.context.socket(zmq.REQ)
@@ -16,19 +14,21 @@ class client_buffer():
         self.current_buffer = 1
         self.max_buffer = buffer_size
         self.my_buffer = [buffer_size]
+        self.BUFF_REQ = "GET buffered"
+        self.MAN_req = "GET manifest"
     
     def start(self):
-        self.socket.send_string(MAN_REQ)
+        self.socket.send_string(self.MAN_REQ)
         self.my_manifest = self.socket.recv()
         print("Received manifest from server")
 
     def run(self):
         self.start()
         while True:
-            self.socket.send_string(BUFF_REQ)
+            self.socket.send_string(self.BUFF_REQ)
             reply = self.socket.recv()
             self.my_buffer[self.current_buffer] = reply
-            print(f"Received reply {BUFF_REQ} [ {reply} ]")
+            print(f"Received reply {self.BUFF_REQ} [ {reply} ]")
             self.current_buffer = (self.current_buffer + 1) % self.max_buffer
 
 
