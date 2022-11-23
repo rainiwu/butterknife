6f275b1682c7423d4a6c10eb21a0fc0281d566fb
ltakai <ltakai@ucsd.edu>
2022-11-22 20:58:03 -0800

    Update manifest.py

--- butter/butter/manifest.py
+++ butter/butter/manifest.py
@@ -1,5 +1,7 @@
-class manifest(object):
-    def __init__(self, totalChunks=0, framePerChunks=0, frameRate=0):
+class manifest:
+    def __init__(self, totalChunks=0, framePerChunks=0, frameRate=0, buff=0):
         self.totalChunks = totalChunks
         self.framePerChunk = framePerChunks
         self.frameRate = frameRate
+        #0 for unbuffered, 1 for buffered
+        self.buff = buff
