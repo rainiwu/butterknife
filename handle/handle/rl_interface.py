9b20c3dc386a6508062b310a2d8a20f57dcabc3f
Raini Wu <rainiwu01@gmail.com>
2022-11-28 01:53:54 -0800

    change to binding

--- handle/handle/rl_interface.py
+++ handle/handle/rl_interface.py
@@ -3,14 +3,20 @@ import zmq.asyncio
 import asyncio
 
 ADDR = "ipc:///dev/shm/priorities"
+
+
 class rl_interface:
-    def __init__(self, address: str = "tcp://localhost:5557", address_srs = "ipc:///dev/shm/priorities"):
+    def __init__(
+        self,
+        address: str = "tcp://localhost:5557",
+        address_srs="ipc:///dev/shm/priorities",
+    ):
         # Initialize socket
         self.context: zmq.asyncio.Context = zmq.asyncio.Context(1)
         self.socket: zmq.asyncio.Socket = self.context.socket(zmq.REQ)
-        self.socket_srs: zmq.asyncio.Socket = self.context.socket(zmq.PUB) 
+        self.socket_srs: zmq.asyncio.Socket = self.context.socket(zmq.PUB)
         self.socket.connect(address)
-        self.socket_srs.connect(address_srs)
+        self.socket_srs.bind(address_srs)
 
         self.prioritized_id = "70"
 
@@ -27,16 +33,17 @@ class rl_interface:
 
     async def report_prioirty(self):
         while True:
-            await self.socket_srs.send(int(self.prioritized_id).to_bytes(4, 'little'))
+            await self.socket_srs.send(int(self.prioritized_id).to_bytes(4, "little"))
             await asyncio.sleep(0.001)
             print("Priority sent to srsRAN: " + self.prioritized_id)
-            print(int(self.prioritized_id).to_bytes(4, 'little'))
+            print(int(self.prioritized_id).to_bytes(4, "little"))
 
     def run(self):
         loop = asyncio.new_event_loop()
         loop.create_task(self.get_priority())
         loop.create_task(self.report_prioirty())
         loop.run_forever()
+
 
 rli = rl_interface("tcp://localhost:5557", "ipc:///dev/shm/priorities")
 rli.run()
