22e197dc00c696d3b88983d3dade6a6bae394f61
j613 <jimmyvillani@gmail.com>
2022-11-27 18:00:57 -0800

    Minor server changes

--- butter/butter/server.py
+++ butter/butter/server.py
@@ -44,37 +44,39 @@ class VideoServer:
         self.QoEdict = {}
 
         self.running = True
+
+        print(f"Server Started")
     
     async def streamer(self) -> None:
         while self.running:
             #  Wait for next request from client
             message = await self.socket.recv()
-            print(f"Message Received: {message}")
+            #print(f"Message Received: {message}")
 
             if message.decode("utf-8") == MAN_REQ_UNBUFF:
                 #print(f"im not stupid!")
                 # For buffered video, send the buffered manifest. 
                 mani = Manifest(self.myChunks, 1, self.myFramesPerSecond, 0)
-                self.socket.send_pyobj(mani)
-                print(f"Unbuffered Manifest Sent")
+                await self.socket.send_pyobj(mani)
+                print(f"Client Connected, Unbuffered Manifest Sent. ")
             elif message.decode("utf-8") == MAN_REQ_BUFF:
                 # For buffered video, send the buffered manifest. 
                 mani = Manifest(self.myChunks, self.myFramesPerChunk, self.myFramesPerSecond, 1)
-                self.socket.send_pyobj(mani)
-                print(f"Buffered Manifest Sent")
+                await self.socket.send_pyobj(mani)
+                print(f"Client Connected, Buffered Manifest Sent")
             elif UNBUFF_REQ in message.decode("utf-8") :
-                self.socket.send(os.urandom(int(1*self.myBytesPerFrame)))
+                await self.socket.send(os.urandom(int(1*self.myBytesPerFrame)))
                 self.QoEdict.update({"70": message.decode("utf-8").split()[-1]})
-                print(f"Unbuffered Chunk Sent.")
+                #print(f"Unbuffered Chunk Sent.")
             elif BUFF_REQ in message.decode("utf-8") :
-                self.socket.send(os.urandom(int(self.myFramesPerChunk*self.myBytesPerFrame)))
+                await self.socket.send(os.urandom(int(self.myFramesPerChunk*self.myBytesPerFrame)))
                 self.QoEdict.update({"71": message.decode("utf-8").split()[-1]})
-                print(f"Buffered Chunk Sent.")
+                #print(f"Buffered Chunk Sent.")
             else:
                 print(f"Malformed request received.")
 
-            print(f"Dictionary of QoEs:")
-            print(self.QoEdict)
+            #print(f"Dictionary of QoEs:")
+            #print(self.QoEdict)
 
 
     async def sendQoE(self) -> None:
@@ -84,8 +86,9 @@ class VideoServer:
 
             if message.decode("utf-8") == QOE_DICT_REQ:
                 # Send the QoE dictionary
-                self.socketRL.send_pyobj(self.QoEdict)
-                print(f"Dictionary Sent")
+                await self.socketRL.send_pyobj(self.QoEdict)
+                print(f"Dictionary Sent to Model:")
+                print(self.QoEdict)
         
 
     def run(self):
