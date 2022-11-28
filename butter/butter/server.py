c8710a27027a42066bf2a922daa3594104541b48
j613 <jimmyvillani@gmail.com>
2022-11-27 20:03:11 -0800

    Better comms with application

--- butter/butter/server.py
+++ butter/butter/server.py
@@ -11,24 +11,26 @@ import asyncio
 from manifest import Manifest
 
 
-MAN_REQ_BUFF = "GET manifest buffered"
-MAN_REQ_UNBUFF = "GET manifest unbuffered"
-BUFF_REQ = "GET buffered"
-UNBUFF_REQ = "GET unbuffered"
-
-QOE_DICT_REQ = "GET qoedict"
-
-CHUNKS = 25
-FRAMESPERCHUNK = 120
-FRAMESPERSECOND = 30
-BYTESPERFRAME = 10e3
-
-
 class VideoServer:
+    MAN_REQ_BUFF = "GET manifest buffered"
+    MAN_REQ_UNBUFF = "GET manifest unbuffered"
+    BUFF_REQ = "GET buffered"
+    UNBUFF_REQ = "GET unbuffered"
 
-    def __init__(self, address="tcp://*:5555", chunks = 25, framesPerChunk = 120, framesPerSecond = 30, bytesPerFrame = 10e3):
+    QOE_DICT_REQ = "GET qoedict"
+    QOE_GET_NUM = "GET quantity"
+
+    ADDRESS = "tcp://*:5555"
+    CHUNKS = 25
+    FRAMESPERCHUNK = 120
+    FRAMESPERSECOND = 30
+    BYTESPERFRAME = 10e3
+
+    NUMBER_OF_CLIENTS = 1
+
+    def __init__(self, address = ADDRESS, chunks = CHUNKS, framesPerChunk = FRAMESPERCHUNK, framesPerSecond = FRAMESPERSECOND, bytesPerFrame = BYTESPERFRAME, numClients = NUMBER_OF_CLIENTS):
         # Create socket for clients
-        self.context = zmq.asyncio.Context(1)
+        self.context = zmq.asyncio.Context()
         self.socket = self.context.socket(zmq.REP)
         self.socket.bind(address)
 
@@ -40,6 +42,7 @@ class VideoServer:
         self.myFramesPerChunk = framesPerChunk
         self.myFramesPerSecond = framesPerSecond
         self.myBytesPerFrame = bytesPerFrame
+        self.myNumClients = numClients
         
         self.QoEdict = {}
 
@@ -52,39 +55,54 @@ class VideoServer:
             #  Wait for next request from client
             message = await self.socket.recv()
             #print(f"Message Received: {message}")
+            messageDecoded = message.decode("utf-8")
 
-            if message.decode("utf-8") == MAN_REQ_UNBUFF:
+            # Manifest sending depending on request
+            if self.MAN_REQ_UNBUFF in messageDecoded:
                 #print(f"im not stupid!")
                 # For buffered video, send the buffered manifest. 
                 mani = Manifest(self.myChunks, 1, self.myFramesPerSecond, 0)
                 await self.socket.send_pyobj(mani)
                 print(f"Client Connected, Unbuffered Manifest Sent. ")
-            elif message.decode("utf-8") == MAN_REQ_BUFF:
+            elif self.MAN_REQ_BUFF in messageDecoded:
                 # For buffered video, send the buffered manifest. 
                 mani = Manifest(self.myChunks, self.myFramesPerChunk, self.myFramesPerSecond, 1)
                 await self.socket.send_pyobj(mani)
                 print(f"Client Connected, Buffered Manifest Sent")
-            elif UNBUFF_REQ in message.decode("utf-8") :
+
+            # Sending video depending on request
+            elif self.UNBUFF_REQ in messageDecoded:
                 await self.socket.send(os.urandom(int(1*self.myBytesPerFrame)))
-                self.QoEdict.update({"70": message.decode("utf-8").split()[-1]})
+                splitMessage = messageDecoded.split()
+                self.QoEdict.update({splitMessage[-2]: splitMessage[-1]})
                 #print(f"Unbuffered Chunk Sent.")
-            elif BUFF_REQ in message.decode("utf-8") :
+            elif self.BUFF_REQ in messageDecoded:
                 await self.socket.send(os.urandom(int(self.myFramesPerChunk*self.myBytesPerFrame)))
-                self.QoEdict.update({"71": message.decode("utf-8").split()[-1]})
+                splitMessage = messageDecoded.split()
+                self.QoEdict.update({splitMessage[-2]: splitMessage[-1]})
                 #print(f"Buffered Chunk Sent.")
+
+            # If the request doesn't match any of the expected forms, don't use.
             else:
                 print(f"Malformed request received.")
 
-            #print(f"Dictionary of QoEs:")
-            #print(self.QoEdict)
+            print(f"Dictionary of QoEs:")
+            print(self.QoEdict)
 
 
     async def sendQoE(self) -> None:
         while self.running:
             #  Wait for next request from client
             message = await self.socketRL.recv()
+            messageDecoded = message.decode("utf-8")
 
-            if message.decode("utf-8") == QOE_DICT_REQ:
+            if messageDecoded == self.QOE_GET_NUM:
+                if len(self.QoEdict) == self.myNumClients:
+                    await self.socketRL.send_string(str(self.myNumClients))
+                else:
+                    await self.socketRL.send_string("-1")
+            
+            if messageDecoded == self.QOE_DICT_REQ:
                 # Send the QoE dictionary
                 await self.socketRL.send_pyobj(self.QoEdict)
                 print(f"Dictionary Sent to Model:")
@@ -102,6 +120,6 @@ class VideoServer:
         
         
 if __name__ == '__main__':
-    vidServ = VideoServer("tcp://*:5555", CHUNKS, FRAMESPERCHUNK, FRAMESPERSECOND, BYTESPERFRAME)
+    vidServ = VideoServer()
     vidServ.run()
    
