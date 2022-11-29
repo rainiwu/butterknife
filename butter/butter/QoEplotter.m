fecef2ac15338b0f717b0a0790b03038adf75ca7
j613 <jimmyvillani@gmail.com>
2022-11-29 11:27:08 -0800

    Adding parser to server.

--- butter/butter/QoEplotter.m
+++ butter/butter/QoEplotter.m
@@ -3,6 +3,7 @@ QoEdata = readmatrix("QoEcsv_2022-11-27_23-01-13.csv");
 
 %% Plot Data
 
+% Start at a later value since first row is
 startVal = 500;
 
 figure(1);
@@ -12,9 +13,11 @@ plot(QoEdata(startVal:end,1),QoEdata(startVal:end,2),'LineWidth',2);
 plot(QoEdata(startVal:end,1),QoEdata(startVal:end,3),'LineWidth',2);
 plot(QoEdata(startVal:end,1),QoEdata(startVal:end,4),'LineWidth',2);
 plot(QoEdata(startVal:end,1),QoEdata(startVal:end,5),'LineWidth',2);
-title('QoE Metrics');
+title('Quality of Experience vs Time');
 xlabel('Time');
 ylabel('QoE');
-legend("70","71","72","73");
+legend("High Throughput Buffered Video","Low Throughput Buffered Video",...
+    "High Throughput Unbuffered Video","Low Throughput Unbuffered Video",...
+    'Location','southeast');
 grid minor;
 hold off;
